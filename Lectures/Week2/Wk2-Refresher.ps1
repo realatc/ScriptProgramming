7df99cfef8697c85dc3fb1f17de0c896f1aef17d
Lance Hundt <lance.hundt@ung.edu>
2023-08-27 18:00:11 -0400

    Wk2 Lecture code

--- Lectures/Week2/Wk2-Refresher.ps1
+++ Lectures/Week2/Wk2-Refresher.ps1
@@ -8,8 +8,9 @@ $var2 = "Go Nighthawks!"
 $var3 = $False
 $var4 = 3.14
 $var5 = $("UNG","Nigel",22,$True)
-$var4.GetType()
+$var5.GetType()
+
 
 ### Search for a CMDLET that prompts and reads user input?
 $userInput = Read-Host -Prompt "Enter a number?"
-Write-Host "The Number you entered was: $userInput"
+Write-Host "The number you entered was: $userInput"
