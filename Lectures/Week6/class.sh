538d2d52760080bc487c6182d4ca5f2a06504e22
lhundt-ung <lance.hundt@ung.edu>
2023-10-04 09:09:27 -0400

    Upload Assignment 5

--- Lectures/Week6/class.sh
+++ Lectures/Week6/class.sh
@@ -3,25 +3,10 @@
 # Write a script that satisfies the following
 # 1. Output todays date
 # 2. Variable to store Target IP address passed as argument
-# 3. Function that runs nmap against target IP address and returns Open Ports (include column headers)
-# 4. Function that runs nmap against target IP address and responds back if system is running "VNC (protocol 3.3)"
+# 3. Function that runs nmap against target IP address and 
+# returns Open Ports (include column headers)
+# 4. Function that runs nmap against target IP address and 
+# responds back if system is running "VNC (protocol 3.3)"
 # 5. Run functions and output to text file
 
-date
-
-var=$1
-echo $var
-
-function openports {
-   nmap $var | grep -B 1 'open'  
-}
-openports > log.txt
-
-function vnc {
-    var2=$(nmap -sV $var | grep 'vnc')
-    if [[ $var2 = *"vnc"* ]]; then
-        echo $var2
-    fi 
-}
-vnc >> log.txt
 
